8db7cbdbfdaac14d5ad182838f8c0e6333549f99
VALLO RANT <vallorant9@gmail.com>
2025-11-16 14:09:54 +0000

    apiKey: "AIzaSyD2ztkcZHDUVyx6Dz8AIogKfmOIfTb0RoM",   authDomain: "studio

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TODO: YOUR_API_KEY",
-  authDomain: "TODO: YOUR_AUTH_DOMAIN",
-  projectId: "TODO: YOUR_PROJECT_ID",
-  storageBucket: "TODO: YOUR_STORAGE_BUCKET",
-  messagingSenderId: "TODO: YOUR_MESSAGING_SENDER_ID",
-  appId: "TODO: YOUR_APP_ID"
+  apiKey: "AIzaSyD2ztkcZHDUVyx6Dz8AIogKfmOIfTb0RoM",
+  authDomain: "studio-1142459954-28867.firebaseapp.com",
+  projectId: "studio-1142459954-28867",
+  storageBucket: "studio-1142459954-28867.appspot.com",
+  messagingSenderId: "394792340205",
+  appId: "1:394792340205:web:d949af5f726e4d79481ae4"
 };
 
 // Initialize Firebase
