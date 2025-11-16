37f0a72069c94a2e88081369e4e21f38fb6a8b94
VALLO RANT <vallorant9@gmail.com>
2025-11-16 15:07:56 +0000

    PUBLIC_API_KEY PUBLIC_AUTH_DOMAIN PUBLIC_PROJECT_ID PUBLIC_STORAGE_BUCKE

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,12 +5,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD2ztkcZHDUVyx6Dz8AIogKfmOIfTb0RoM",
-  authDomain: "studio-1142459954-28867.firebaseapp.com",
-  projectId: "studio-1142459954-28867",
-  storageBucket: "studio-1142459954-28867.appspot.com",
-  messagingSenderId: "394792340205",
-  appId: "1:394792340205:web:d949af5f726e4d79481ae4"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID
 };
 
 // Initialize Firebase
