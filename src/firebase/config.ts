15d4a7208788d6bcc9575d1e8c42e3674e02d268
VALLO RANT <vallorant9@gmail.com>
2025-11-19 02:30:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1142459954-28867",
+  "appId": "1:394792340205:web:d949af5f726e4d79481ae4",
+  "apiKey": "AIzaSyD2ztkcZHDUVyx6Dz8AIogKfmOIfTb0RoM",
+  "authDomain": "studio-1142459954-28867.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "394792340205"
+};
